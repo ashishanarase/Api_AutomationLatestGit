5f6fd04dec7420d041de01ed3b3dade30a8ab532
Ashish Anarase <anaraseashish@gmail.com>
2024-11-21 21:36:48 +0530

    New

--- Api_AutomationLatestGit/src/test/java/twenty24/May81.java
+++ Api_AutomationLatestGit/src/test/java/twenty24/May81.java
@@ -1,16 +1,7 @@
 package twenty24;
 
-public class May81 extends May8 {
+public class May81 {
 
-	@Override
-	public void method1() {
-		super.method1();
-	}
-
-	@Override
-	public void method1(int a) {
-		super.method1(a);
-	}
-
+	
 	
 }
